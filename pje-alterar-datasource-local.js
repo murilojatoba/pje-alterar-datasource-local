cd2c82e54dfd467dd4d1741f95eacb090446842f
Murilo Jatobá <murilojatoba@gmail.com>
2023-05-04 10:16:12 -0300

    Inclusão das bases do pje-legado e apontar para a pasta correta dos datasources.

--- pje-alterar-datasource-local.js
+++ pje-alterar-datasource-local.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const process = require('process');
 
 const pathPje = '/usr/local/pje/';
-const pathDS = pathPje + 'workspace-eclipse/Datasources/teste/'
+const pathDS = pathPje + 'workspace-eclipse/Datasources/'
 const prefixoPrincipal = 'pjekz-ds';
 const prefixoSeguranca = 'pjekz-seguranca-ds';
+const prefixoLegado1Grau = 'pje-legado-1grau-ds';
+const prefixoLegado2Grau = 'pje-legado-2grau-ds';
 const sufixoLocahost = '-LOCALHOST';
 const sufixoDevlocal = '-DEVLOCAL';
 const sufixoDev = '-DEV';
@@ -19,6 +21,16 @@ const dsSeguranca = () => prefixoSeguranca + extensao;
 const dsSegurancaLocalhost = () => prefixoSeguranca + sufixoLocahost + extensao;
 const dsSegurancaDev = () => prefixoSeguranca + sufixoDev + extensao;
 const dsSegurancaDevlocal = () => prefixoSeguranca + sufixoDevlocal + extensao;
+
+const dsLegado1Grau = () => prefixoLegado1Grau + extensao;
+const dsLegado1GrauLocalhost = () => prefixoLegado1Grau + sufixoLocahost + extensao;
+const dsLegado1GrauDev = () => prefixoLegado1Grau + sufixoDev + extensao;
+const dsLegado1GrauDevlocal = () => prefixoLegado1Grau + sufixoDevlocal + extensao;
+
+const dsLegado2Grau = () => prefixoLegado2Grau + extensao;
+const dsLegado2GrauLocalhost = () => prefixoLegado2Grau + sufixoLocahost + extensao;
+const dsLegado2GrauDev = () => prefixoLegado2Grau + sufixoDev + extensao;
+const dsLegado2GrauDevlocal = () => prefixoLegado2Grau + sufixoDevlocal + extensao;
 
 async function renomearArquivo(oldFile, newFile) {
     console.log('   Renomeando', oldFile, 'para', newFile);
@@ -41,6 +53,20 @@ async function renomearBaseSeguranca(de, para) {
     await renomearArquivo(para, dsSeguranca());
 }
 
+async function renomearBaseLegado1Grau(de, para) {
+    console.log(`Alterando ambiente de ${de} para ${para}.`);
+
+    await renomearArquivo(dsLegado1Grau(), de);
+    await renomearArquivo(para, dsLegado1Grau());
+}
+
+async function renomearBaseLegado2Grau(de, para) {
+    console.log(`Alterando ambiente de ${de} para ${para}.`);
+
+    await renomearArquivo(dsLegado2Grau(), de);
+    await renomearArquivo(para, dsLegado2Grau());
+}
+
 const paramOrigem = process.argv[2] ? process.argv[2].toLowerCase() : null;
 const paramDestino = process.argv[3] ? process.argv[3].toLowerCase() : null;
 
@@ -51,18 +77,26 @@ if(!paramOrigem || !paramDestino){
 
 let arqOrigemPrincipal;
 let arqOrigemSeguranca;
+let arqOrigemLegado1Grau;
+let arqOrigemLegado2Grau;
 switch (paramOrigem) {
     case 'localhost':
         arqOrigemPrincipal = dsPrincipalLocalhost();
         arqOrigemSeguranca = dsSegurancaLocalhost();
+        arqOrigemLegado1Grau = dsLegado1GrauLocalhost();
+        arqOrigemLegado2Grau = dsLegado2GrauLocalhost();
         break;
     case 'dev':
         arqOrigemPrincipal = dsPrincipalDev();
         arqOrigemSeguranca = dsSegurancaDev();
+        arqOrigemLegado1Grau = dsLegado1GrauDev();
+        arqOrigemLegado2Grau = dsLegado2GrauDev();
         break;
     case 'devlocal':
         arqOrigemPrincipal = dsPrincipalDevlocal();
         arqOrigemSeguranca = dsSegurancaDevlocal();
+        arqOrigemLegado1Grau = dsLegado1GrauDevlocal();
+        arqOrigemLegado2Grau = dsLegado2GrauDevlocal();
         break;
     default:
         break;
@@ -70,18 +104,26 @@ switch (paramOrigem) {
 
 let arqDestinoPrincipal;
 let arqDestinoSeguranca;
+let arqDestinoLegado1Grau;
+let arqDestinoLegado2Grau;
 switch (paramDestino) {
     case 'localhost':
         arqDestinoPrincipal = dsPrincipalLocalhost();
         arqDestinoSeguranca = dsSegurancaLocalhost();
+        arqDestinoLegado1Grau = dsLegado1GrauLocalhost();
+        arqDestinoLegado2Grau = dsLegado2GrauLocalhost();
         break;
     case 'dev':
         arqDestinoPrincipal = dsPrincipalDev();
         arqDestinoSeguranca = dsSegurancaDev();
+        arqDestinoLegado1Grau = dsLegado1GrauDev();
+        arqDestinoLegado2Grau = dsLegado2GrauDev();
         break;
     case 'devlocal':
         arqDestinoPrincipal = dsPrincipalDevlocal();
         arqDestinoSeguranca = dsSegurancaDevlocal();
+        arqDestinoLegado1Grau = dsLegado1GrauDevlocal();
+        arqDestinoLegado2Grau = dsLegado2GrauDevlocal();
         break;
     default:
         break;
@@ -90,6 +132,8 @@ switch (paramDestino) {
 async function executar(){
     await renomearBasePrincipal(arqOrigemPrincipal, arqDestinoPrincipal);
     await renomearBaseSeguranca(arqOrigemSeguranca, arqDestinoSeguranca);
+    await renomearBaseLegado1Grau(arqOrigemLegado1Grau, arqDestinoLegado1Grau);
+    await renomearBaseLegado2Grau(arqOrigemLegado2Grau, arqDestinoLegado2Grau);
     console.log('Ambiente configurado!');
 }
 
